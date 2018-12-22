97f4054ce4e0bcfe665722401569aa4ce1830e19
Kevinmotanya <42073382+Kevinmotanya@users.noreply.github.com>
2018-12-22 10:29:51 -0600

    jserver

---
+++ app/Routing/apiRoutes.js
@@ -0,0 +1,34 @@
+var friends = require("../data/friends");
+
+module.exports = function(app){
+    app.get("/api/friends", function(req,res) {
+        res.json(friends);
+    });
+
+    app.post("/api/friends", function(req,res) {
+        var newFriend = req.body;
+        var match = friends[0];
+        var diffs = [];
+        for(var i=0;i<friends.length;i++){
+            var totalDiff = 0;
+            for (var x=0;x<friends[i].scores.length;x++){
+                var compare = Math.abs(newFriend.scores[x] - friends[i].scores[x]);
+                totalDiff += compare;
+            };
+            diffs.push(totalDiff);
+        };
+        var least = diffs.min();
+        for (var i=0;i<diffs.length;i++){
+            if(diffs[i]===least){
+                match=friends[i];
+                return(match);
+            };
+            return(match);
+        };
+
+
+        friends.push(newFriend);
+
+        res.json(match);
+    });
+};
---
+++ app/data/friends.js
@@ -0,0 +1,35 @@
+var friends = [
+    {
+        "name":"Zedy",
+        "photo":"link",
+        "scores":[
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    },{
+        "name":"jbu",
+        //"photo":"https://www.rothamsted.ac.uk/sites/default/files/person-profile-images/jbu.jpg",
+        "scores":[
+            3,
+            2,
+            5,
+            1,
+            2,
+            2,
+            5,
+            3,
+            3,
+            2
+        ]
+    }
+];
+
+module.exports = friends;
