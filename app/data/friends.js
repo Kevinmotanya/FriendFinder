dd787d555ea2672a4320562f195a291e0666c3e4
Kevinmotanya <42073382+Kevinmotanya@users.noreply.github.com>
2018-12-22 22:11:30 -0600

    deploy message

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,7 @@
 var friends = [
     {
-        "name":"Zedy",
-        "photo":"link",
+        "name":"Ahmed",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
         "scores":[
             5,
             1,
@@ -13,23 +13,42 @@ var friends = [
             5,
             4,
             1
-        ]
-    },{
-        "name":"jbu",
-        //"photo":"https://www.rothamsted.ac.uk/sites/default/files/person-profile-images/jbu.jpg",
+          ]
+      },
+      
+      {
+        "name":"Ahmed",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
         "scores":[
-            3,
-            2,
+            5,
+            1,
+            4,
+            4,
             5,
             1,
             2,
+            5,
+            4,
+            1
+          ]
+      }
+      ,{
+        "name":"Ahmed",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores":[
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
             2,
             5,
-            3,
-            3,
-            2
-        ]
-    }
+            4,
+            1
+          ]
+      }
+      
 ];
 
 module.exports = friends;
